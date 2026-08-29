628da318bc439fa9ba31abfe215b89a8b63462f6
Roberto M Pinetti <robertopinetti@gmail.com>
2026-08-29 12:19:40 -0300

    Add initial implementation of battery, radio, and time services; update CMake configuration and Nordico theme colors

--- runesos-ui/theme/theme_nordico.h
+++ runesos-ui/theme/theme_nordico.h
@@ -4,11 +4,11 @@
 #include "lvgl.h"
 
 /* Paleta nórdica */
-#define RUNESOS_COLOR_BG       lv_color_hex(0x1a1a2e)  /* azul-noite */
-#define RUNESOS_COLOR_SURFACE  lv_color_hex(0x16213e)  /* azul profundo */
+#define RUNESOS_COLOR_BG       lv_color_hex(0x0d1b2a)  /* azul fiorde */
+#define RUNESOS_COLOR_SURFACE  lv_color_hex(0x495057)  /* cinza forja */
 #define RUNESOS_COLOR_ACCENT   lv_color_hex(0xd4af37)  /* dourado runas */
-#define RUNESOS_COLOR_ICE      lv_color_hex(0x0f3460)  /* azul gelo */
-#define RUNESOS_COLOR_TEXT     lv_color_hex(0xeeeeee)  /* branco gelo */
+#define RUNESOS_COLOR_ICE      lv_color_hex(0x66fcf1)  /* ciano glacial */
+#define RUNESOS_COLOR_TEXT     lv_color_hex(0xf8f9fa)  /* branco runico */
 
 void runesos_theme_apply(lv_obj_t *scr);
 
